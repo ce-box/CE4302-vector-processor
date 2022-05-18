dfcdbe87fc76c1333065c8c3fe20c664a9a619b6
Esteban Alvarado <estalvgs1999@estudiantec.cr>
2022-05-18 16:40:06 -0600

    [add] docs and diagrams

--- processor/tests/geo/test-cases/data-processing/data.s
+++
@@ -1,7 +0,0 @@
-add r0, r0, #0
-add r1, r0, #5
-add r2, r1, #4
-sub r3, r2, r1
-and r5, r2, r3
-orr r6, r2, r3
-mov r8, #0xf
--- processor/tests/geo/test-cases/memory/memory.s
+++
@@ -1,5 +0,0 @@
-add r3, r0, #0x2c
-ldr r5, [r0]
-mov r4, #0, #4
-str r3, [r4]
-add r6, r0, #0xf
